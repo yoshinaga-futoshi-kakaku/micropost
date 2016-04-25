ae2c16e77cb5a7687b5af4497e018699eecb60b7
futoshi <yoshinaga.futoshi.kakaku@gmail.com>
2016-04-25 06:27:16 +0000

    add install setup

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,14 @@ module Microposts
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
